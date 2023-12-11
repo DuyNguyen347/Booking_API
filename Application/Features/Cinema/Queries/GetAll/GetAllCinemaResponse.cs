be3bc101100d80a17461e759461169b3dc56daac
NguyenDucDuy <ducduynguyen347@gmail.com>
2023-12-11 08:48:46 +0700

    add feature poster and image cinema and fix edit film

--- Application/Features/Cinema/Queries/GetAll/GetAllCinemaResponse.cs
+++ Application/Features/Cinema/Queries/GetAll/GetAllCinemaResponse.cs
@@ -14,6 +14,10 @@ namespace Application.Features.Cinema.Queries.GetAll
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? City { get; set; }
+        public string? Hotline { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+        public string? Address { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime? LastModifiedOn { get; set; }
 
--- Infrastructure/Migrations/20231210171253_addPosterTableAndAddCinameImage.Designer.cs
+++ Infrastructure/Migrations/20231210171253_addPosterTableAndAddCinameImage.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231210171253_addPosterTableAndAddCinameImage")]
+    partial class addPosterTableAndAddCinameImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -432,10 +435,6 @@ namespace Infrastructure.Migrations
                         .HasColumnType("bigint");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<long>("Id"));
-
-                    b.Property<string>("Address")
-                        .HasColumnType("nvarchar(MAX)")
-                        .HasColumnName("address");
 
                     b.Property<string>("City")
                         .HasColumnType("nvarchar(50)")
